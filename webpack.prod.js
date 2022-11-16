e225c7ee101a543dc44984c5737a19d408dddf15
Gilbert Christyano Day <107147855+GilbertDay@users.noreply.github.com>
2022-11-16 21:25:06 +0700

    Fixed Bugs

--- webpack.prod.js
+++ webpack.prod.js
@@ -27,6 +27,9 @@ module.exports = merge(common, {
           MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
+            options: {
+              url: false,
+            },
           },
         ],
       },
